ca0cf05a235d1267dbd5e2ec55854618b642d9a7
Alexey S. Kompaniets <kompadre@gmail.com>
2021-08-24 02:09:20 +0200

    Some more multilang stuff

--- view/work.php
+++ view/work.php
@@ -65,9 +65,9 @@
             <h3 class="center"><?= _('Do you want to be part of the Damavis team?') ?></h3>
         </div>
         <form>
-            <a class="cta" href="https://docs.google.com/forms/d/e/1FAIpQLSdGlELnI193ckVAWnla2Gw8v_zUHEGNQQnYXUuP5JGp3wbrrw/viewform"><?= _('Apply now') ?></a>
+            <a class="cta" rel="noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSdGlELnI193ckVAWnla2Gw8v_zUHEGNQQnYXUuP5JGp3wbrrw/viewform"><?= _('Apply now') ?></a>
         </form>
-        <a class="contact-jobs-link" href="https://docs.google.com/forms/d/e/1FAIpQLSdGlELnI193ckVAWnla2Gw8v_zUHEGNQQnYXUuP5JGp3wbrrw/viewform">
+        <a class="contact-jobs-link" rel="noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSdGlELnI193ckVAWnla2Gw8v_zUHEGNQQnYXUuP5JGp3wbrrw/viewform">
             > <?= _('Do you want to work with us?') ?>
         </a>
     </div>
