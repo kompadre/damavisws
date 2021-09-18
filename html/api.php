056a07ee90e21335b3b6bb45c4343629ada8dd1e
Alexey S. Kompaniets <kompadre@gmail.com>
2021-09-18 19:20:12 +0200

    Changes related to https://docs.google.com/document/d/1l579Enm0pNNVII8lTiO40Niy1_HJH96wCDJEyRqYBWY

--- html/api.php
+++ html/api.php
@@ -1,1 +1,12 @@
-../api.php
+<?php
+$out = ['ok' => true];
+if (empty($_POST)) {
+    $out = ['ok' => false, 'error' => 'POST only'];
+}
+switch($_GET['action']) {
+    case 'sendmail':
+        $out = var_export($_POST, true);
+        break;
+}
+header('Content-Type: application/json');
+echo json_encode($out);
