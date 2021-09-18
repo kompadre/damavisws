2cb357e7708200ebfd2f0229a4e0d3b34e044e1d
Alexey S. Kompaniets <kompadre@gmail.com>
2021-09-18 19:14:00 +0200

    Changes related to https://docs.google.com/document/d/1l579Enm0pNNVII8lTiO40Niy1_HJH96wCDJEyRqYBWY

--- api.php
+++ api.php
@@ -1,11 +1,11 @@
 <?php
 $out = ['ok' => true];
 if (empty($_POST)) {
-    return json_encode('POST only');
+    $out = ['ok' => false, 'error' => 'POST only'];
 }
 switch($_SERVER['REQUEST_URI']) {
     case 'sendmail':
-        $out = var_export($_POST);
+        $out = var_export($_POST, true);
 }
 header('Content-Type: application/json');
 return json_encode($out);
